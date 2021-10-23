1910a7b1e2f1360b1465f265d67732e77a6b0b27
Glade Snyder <snydergo@pierre.cs.colostate.edu>
2021-10-23 10:12:33 -0600

    added to gitignore to ignore the large data files

--- build.sbt
+++ build.sbt
@@ -6,3 +6,10 @@ scalaVersion := "2.13.6"
 
 // set the main class for 'sbt run'
 Compile / run / mainClass := Some("org.movielens.Application")
+
+val sparkVersion = "3.2.0"
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.apache.spark" %% "spark-graphx" % sparkVersion
+)
--- src/main/scala/org/movielens/Application.scala
+++ src/main/scala/org/movielens/Application.scala
@@ -1,10 +1,24 @@
+
 package org.movielens
+
+//import org.apache.spark
+//import org.apache.spark.sql.SparkSession
 
 object Application {
 
   def main(args: Array[String]): Unit = {
-    println("Launching Application with args:")
-    args.foreach(println)
+    println("Launching Application with args:");
+    if(args.length == 0) {
+      println("need to provide spark master location and data csv directory")
+      return
+    }
+    args.foreach(println);
+    val spark_mstr_str = args(0)
+    val csv_dir = args(1)
+    //val sc = SparkSession.builder().master("local").getOrCreate().sparkContext
+    //val spark = SparkSession.builder().master(spark_mstr_str).getOrCreate()
+
+    //val df = spark.read.format("csv").load(csv_dir + "/ratings.csv")
   }
 
 }
